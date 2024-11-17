8f0f4c64c3c9e1160a10408c75d5e60d533a1eff
annatbui <48100681+annatbui@users.noreply.github.com>
2024-11-17 22:06:01 +0000

    update to plot 4 panels

--- cdft/plot_utils.py
+++ cdft/plot_utils.py
@@ -36,6 +36,51 @@ def configure_plot(zbins):
     plt.tight_layout()
     return fig, ax
 
+def configure_4panels(zbins):
+    fig, ax = plt.subplots(2, 2, figsize=(8, 6.5), sharex=True)
+    ax[0,0].set_xlim(zbins[0], zbins[-1])
+    ax[0,1].set_ylim(-17, 2)
+    ax[1,0].set_ylim(-0.003, 0.003)
+    ax[1,1].set_ylim(-15, 15)
+    
+    ax[0,0].set_ylabel(r'$\rho(z)$ [$\mathrm{\AA}^{-3}$]')
+    ax[0,1].set_ylabel(r'$\beta[\mu - V_{\mathrm{ext}}(z) - q \phi(z)]$')
+    ax[1,0].set_ylabel(r'$n(z)$ [$e\mathrm{\AA}^{-3}$]')
+    ax[1,1].set_ylabel(r'$-\beta e \Delta \phi(z)$')
+    
+    
+    ax[1,0].set_xlabel(r'$z$ [$\mathrm{\AA}$]')
+    ax[1,1].set_xlabel(r'$z$ [$\mathrm{\AA}$]')
+    ax[0,1].yaxis.set_label_position("right")
+    ax[0,1].yaxis.tick_right()
+    ax[1,1].yaxis.set_label_position("right")
+    ax[1,1].yaxis.tick_right()
+    
+    ax[0,0].grid(which="major", ls="dashed", dashes=(1, 3), lw=0.5, zorder=0)
+    ax[0,1].grid(which="major", ls="dashed", dashes=(1, 3), lw=0.5, zorder=0)
+    ax[1,0].grid(which="major", ls="dashed", dashes=(1, 3), lw=0.5, zorder=0)
+    ax[1,1].grid(which="major", ls="dashed", dashes=(1, 3), lw=0.5, zorder=0)
+    ax[0,0].tick_params(direction="in", which="major", length=5, labelsize=13)
+    ax[1,0].tick_params(direction="in", which="major", length=5, labelsize=13)
+    ax[0,1].tick_params(direction="in", which="major", length=5, labelsize=13)
+    ax[1,1].tick_params(direction="in", which="major", length=5, labelsize=13)
+    
+    plt.tight_layout()
+    return fig, ax
+
+def plot_interactive_4panels(fig, ax, zbins, rho_H, rho_O, charge_density, 
+                             restructuring_potential, muloc_H, muloc_O, color_count):
+    display.clear_output(wait=True)
+    ax[0,0].plot(zbins, rho_H, color=color_cycle[color_count] )
+    ax[0,0].plot(zbins, rho_O, ls="--", color=color_cycle[color_count])
+    ax[1,0].plot(zbins, charge_density, color=color_cycle[color_count] )
+    ax[0,1].plot(zbins, muloc_H, color=color_cycle[color_count] )
+    ax[0,1].plot(zbins, muloc_O, ls="--", color=color_cycle[color_count] )
+    ax[1,1].plot(zbins, restructuring_potential, color=color_cycle[color_count] )
+    display.display(fig)
+    
+
+
 def plot_interactive_SR_onetype(fig, ax, zbins, rho, muloc, color_count):
     display.clear_output(wait=True)
     ax[0].plot(zbins, rho, color=color_cycle[color_count])
@@ -62,6 +107,16 @@ def plot_end_SR_onetype(zbins, rho, muloc, ax):
     ax[0].plot(zbins, rho, color='black', lw=2)
     ax[1].plot(zbins, muloc, color='black', lw=2)
     
+    
+
+def plot_end_4panels(zbins, rhoH, rhoO, mulocH, mulocO, ax):
+    display.clear_output(wait=False)
+    ax[0,0].plot(zbins, rhoH, color='black', lw=2)
+    ax[0,0].plot(zbins, rhoO, ls='--', color='black', lw=2)
+    ax[0,1].plot(zbins, mulocH, color='black', lw=2)
+    ax[0,1].plot(zbins, mulocO, ls='--', color='black', lw=2)
+    
+
 def plot_end_SR_twotype(zbins, rhoH, rhoO, mulocH, mulocO, ax):
     display.clear_output(wait=False)
     ax[0].plot(zbins, rhoH, color='black', lw=2)
